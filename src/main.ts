a2c4a131d6fa5570109e8e2310f7cc108494e50e
Tim <timothee.varaillas@gmail.com>
2023-02-23 14:18:01 +0100

    @ Correction aliases main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from "vue";
-import App from "@/App.vue";
+import App from "./App.vue";
 
-
+import "./style.css";
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import icons from "@/assets/icons";
+import icons from "./assets/icons";
 library.add(...icons);
 
 createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
