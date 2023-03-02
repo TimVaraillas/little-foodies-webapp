9cfb0c452d6e15fb3c3d22b14bb61941e1913c76
Tim <timothee.varaillas@gmail.com>
2023-03-02 17:52:21 +0100

    + Initialisation de vue-appolo dans l'application

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import { createApp } from "vue";
+import { createApp, provide, h } from "vue";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
 import App from "./App.vue";
 
 import "./style.css";
@@ -9,4 +11,17 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import icons from "./assets/icons";
 library.add(...icons);
 
-createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
+const cache = new InMemoryCache();
+const apolloClient = new ApolloClient({
+  cache,
+  uri: "http://localhost:4000/graphql",
+});
+
+createApp({
+  setup: () => {
+    provide(DefaultApolloClient, apolloClient);
+  },
+  render: () => h(App),
+})
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
