f0d83fad13bc752edc730a2d2dcc5482543f07fa
Tim <timothee.varaillas@gmail.com>
2023-03-02 17:51:46 +0100

    + Création de l'organisme FoodGrid

---
+++ src/components/organisms/FoodGrid/FoodGridOrganism.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import FoodItemMolecule from "@components/molecules/FoodItem/FoodItemMolecule.vue";
+import TextAtom from "@components/atoms/Text/TextAtom.vue";
+
+const props = defineProps<{
+  foodByCategories: any[];
+}>();
+</script>
+
+<template>
+  <div
+    class="w-full grid grid-cols-12 gap-4 mb-14"
+    v-for="category in props.foodByCategories"
+    :key="category"
+  >
+    <text-atom class="col-span-12 text-center mb-6" size="3xl" weight="bold">{{ category.category }}</text-atom>
+    <food-item-molecule
+      v-for="item in category.food"
+      :key="item"
+      v-bind="item"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
