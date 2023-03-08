04c355a3b068b6d10570604855c6fd81a66b45c5
Tim <timothee.varaillas@gmail.com>
2023-03-08 13:11:20 +0100

    @ Correction main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-import { createApp, provide, h } from "vue";
+import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { DefaultApolloClient } from "@vue/apollo-composable";
-import { ApolloClient, InMemoryCache } from "@apollo/client/core";
 import App from "./App.vue";
 
 import "./style.css";
@@ -12,20 +10,9 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import icons from "./assets/icons";
 library.add(...icons);
 
-const cache = new InMemoryCache();
-const apolloClient = new ApolloClient({
-  cache,
-  uri: "http://localhost:4000/graphql",
-});
-
 const pinia = createPinia();
 
-createApp({
-  setup: () => {
-    provide(DefaultApolloClient, apolloClient);
-  },
-  render: () => h(App),
-})
+createApp(App)
   .use(pinia)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
