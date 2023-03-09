cc35ea7d1061b21f197d29f1bbde3bb91917e41b
Tim <timothee.varaillas@gmail.com>
2023-03-10 00:25:04 +0100

    + Création de la molécule Menu

---
+++ src/components/molecules/Menu/MenuMolecule.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import MenuItemAtom from "@/components/atoms/MenuItem/MenuItemAtom.vue";
+
+import type { MenuItem } from "@/types/menu.type";
+
+const props = defineProps<{
+  menu: MenuItem[][];
+}>();
+</script>
+
+<template>
+  <div class="bg-white divide-y divide-slate-100 rounded-lg">
+    <slot name="prepend"></slot>
+    <ul
+      v-for="(menuGroup, index) in props.menu"
+      :key="index"
+      class="py-1 text-sm text-slate-700"
+    >
+      <menu-item-atom
+        v-for="item of menuGroup"
+        :key="item.name"
+        :item="item"
+      ></menu-item-atom>
+    </ul>
+    <slot name="append"></slot>
+  </div>
+</template>
