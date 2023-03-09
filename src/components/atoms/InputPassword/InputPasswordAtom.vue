9177f6c9bcc502f086cf539d012a2117715e124a
Tim <timothee.varaillas@gmail.com>
2023-03-09 11:07:36 +0100

    + Création de l'atom inputPassword

---
+++ src/components/atoms/InputPassword/InputPasswordAtom.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import { computed, ref, Ref } from "vue";
+
+type Size = "xs" | "sm" | "md" | "lg" | "xl";
+
+interface Props {
+  modelValue?: string;
+  placeholder?: string;
+  prependIcon?: string;
+  viewable?: boolean;
+  clearable?: boolean;
+  bordered?: boolean;
+  size?: Size;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  size: "md",
+  bordered: true,
+  viewable: true,
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const internalValue = computed({
+  get: () => props.modelValue,
+  set: (value) => emit("update:modelValue", value),
+});
+
+const clearValue = () => {
+  internalValue.value = "";
+};
+
+const inputClass = {
+  "px-3 py-1 text-xs": props.size === "xs",
+  "px-3 py-1.5 text-sm": props.size === "sm",
+  "px-4 py-2.5 text-base": props.size === "md",
+  "pl-4 pr-5 py-4 text-lg": props.size === "lg",
+  "pl-4 pr-5 py-6 text-xl": props.size === "xl",
+  "border border-slate-300": props.bordered,
+};
+
+const hidePassword: Ref<boolean> = ref(true);
+</script>
+
+<template>
+  <div
+    class="w-full flex bg-slate-50 text-slate-900 rounded-lg focus-within:ring-4 focus-within:ring-amber-200"
+    :class="inputClass"
+  >
+    <div
+      v-if="props.prependIcon"
+      class="flex items-center pointer-events-none mr-3 text-slate-400"
+    >
+      <font-awesome-icon :icon="props.prependIcon" />
+    </div>
+    <input
+      :type="hidePassword ? 'password' : 'text'"
+      class="grow bg-slate-50"
+      v-model="internalValue"
+      :placeholder="props.placeholder"
+    />
+    <button
+      v-if="props.clearable && internalValue"
+      type="button"
+      class="flex items-center ml-2 text-slate-400 hover:text-slate-800"
+      @click="clearValue"
+    >
+      <font-awesome-icon class="text-xs" icon="fa-solid fa-xmark" />
+    </button>
+    <button
+      v-if="props.viewable"
+      type="button"
+      class="flex items-center ml-2 text-slate-400 hover:text-slate-800"
+      @click="() => (hidePassword = !hidePassword)"
+    >
+      <font-awesome-icon
+        class="text-xs"
+        :icon="hidePassword ? 'fa-solid fa-eye' : 'fa-solid fa-eye-slash'"
+      />
+    </button>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
