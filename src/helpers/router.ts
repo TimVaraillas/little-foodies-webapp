db4f03d10ed96b89e6a03f6fdccd20da4c36cd95
Tim <timothee.varaillas@gmail.com>
2023-03-09 11:41:31 +0100

    @ Redirectirection du router vers la page de login si l'utilisateur n'est pas identifié

--- src/helpers/router.ts
+++ src/helpers/router.ts
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import LoginPage from "@/components/pages/Login/LoginPage.vue";
 import FoodGridPage from "@/components/pages/FoodGrid/FoodGridPage.vue";
 
+import { useAuthStore } from "@/stores/auth.store";
+
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   linkActiveClass: "active",
@@ -10,4 +12,21 @@ export const router = createRouter({
     { path: "/", component: FoodGridPage },
     { path: "/login", component: LoginPage },
   ],
+});
+
+router.beforeEach(async (to) => {
+  const publicPages: string[] = [];
+  const offlinePages: string[] = ["/login"];
+  const authRequired: boolean = ![...publicPages, ...offlinePages].includes(
+    to.path
+  );
+  const authStore = useAuthStore();
+
+  if (authRequired && !authStore.user) {
+    return "/login";
+  }
+
+  if (offlinePages.includes(to.path) && authStore.user) {
+    return "/";
+  }
 });
