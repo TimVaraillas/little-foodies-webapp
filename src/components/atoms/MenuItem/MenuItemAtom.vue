ee968249146406cc3d58743e1887fb65991bc452
Tim <timothee.varaillas@gmail.com>
2023-03-10 00:24:41 +0100

    + Creation de l'atome MenuItem

---
+++ src/components/atoms/MenuItem/MenuItemAtom.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { MenuItem } from "@/types/menu.type";
+
+const props = defineProps<{
+  item: MenuItem;
+}>();
+</script>
+
+<template>
+  <li @click="() => props.item.handler()">
+    <span
+      class="block px-4 py-2 hover:bg-slate-100 cursor-pointer"
+      :class="[...(props.item.color ? [`text-${props.item.color}`] : [])]"
+    >
+      {{ props.item.name }}
+    </span>
+  </li>
+</template>
