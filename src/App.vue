400dbda67bc27e249e446abcbbf3aa0b6bd8e71c
Tim <timothee.varaillas@gmail.com>
2023-03-10 17:44:40 +0100

    @ Alimentation de l'organisme NavBar par des props

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,59 @@
 <script setup lang="ts">
+import { ref, Ref, onMounted, onUnmounted } from "vue";
 import { RouterView } from "vue-router";
+import { storeToRefs } from "pinia";
 
 import NavBarOrganism from "@/components/organisms/NavBar/NavBarOrganism.vue";
+
+import { useAuthStore } from "@/stores/auth.store";
+
+import type { MenuItem } from "@/types/menu.type";
+import type { User } from "@/types/user.type";
+
+const authStore = useAuthStore();
+
+const { user } = storeToRefs(authStore);
+
+const avatarMenu: Ref<MenuItem[][]> = ref([
+  [
+    {
+      name: "Ma famille",
+      handler: () => {},
+    },
+  ],
+  [
+    {
+      name: "Se déconnecter",
+      handler: () => {
+        authStore.logout();
+      },
+      color: "rose-500",
+    },
+  ],
+]);
+
+const windowScrolled: Ref<boolean> = ref(false);
+
+const onScroll = () => {
+  windowScrolled.value = window.scrollY > 10 ? true : false;
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", onScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", onScroll);
+});
 </script>
 
 <template>
-  <nav-bar-organism />
+  <nav-bar-organism
+    class="sticky top-0 z-20 transition-all duration-300"
+    :class="{ 'shadow-md': windowScrolled }"
+    :user="(user as User)"
+    :avatar-menu="avatarMenu"
+  />
   <RouterView />
 </template>
 
--- src/components/organisms/NavBar/NavBarOrganism.vue
+++ src/components/organisms/NavBar/NavBarOrganism.vue
@@ -1,66 +1,25 @@
 <script setup lang="ts">
-import { ref, Ref, onMounted, onUnmounted } from "vue";
-import { storeToRefs } from "pinia";
-
 import AppTitleAtom from "@/components/atoms/AppTitle/AppTitleAtom.vue";
 import AvatarDropdownMolecule from "@/components/molecules/AvatarDropdown/AvatarDropdownMolecule.vue";
-
-import { useAuthStore } from "@/stores/auth.store";
 
 import type { MenuItem } from "@/types/menu.type";
 import type { User } from "@/types/user.type";
 
-const authStore = useAuthStore();
-
 const props = withDefaults(
   defineProps<{
     appTitle?: string;
+    user?: User;
+    avatarMenu?: MenuItem[][];
   }>(),
   {
     appTitle: "Little foodies",
+    avatarMenu: () => [],
   }
 );
-
-const { user } = storeToRefs(authStore);
-
-const menu: Ref<MenuItem[][]> = ref([
-  [
-    {
-      name: "Ma famille",
-      handler: () => {},
-    },
-  ],
-  [
-    {
-      name: "Se déconnecter",
-      handler: () => {
-        authStore.logout();
-      },
-      color: "rose-500",
-    },
-  ],
-]);
-
-const windowScrolled: Ref<boolean> = ref(false);
-
-const onScroll = () => {
-  windowScrolled.value = window.scrollY > 10 ? true : false;
-};
-
-onMounted(() => {
-  window.addEventListener("scroll", onScroll);
-});
-
-onUnmounted(() => {
-  window.removeEventListener("scroll", onScroll);
-});
 </script>
 
 <template>
-  <nav
-    class="bg-amber-100 h-16 sticky top-0 z-20 transition-all duration-300"
-    :class="{ 'shadow-md': windowScrolled }"
-  >
+  <nav class="bg-amber-100 h-16">
     <div
       class="h-full container flex flex-wrap items-center justify-between mx-auto"
     >
@@ -69,7 +28,7 @@ onUnmounted(() => {
         <avatar-dropdown-molecule
           v-if="user"
           :user="(user as User)"
-          :menu="menu"
+          :menu="avatarMenu"
         />
       </div>
     </div>
