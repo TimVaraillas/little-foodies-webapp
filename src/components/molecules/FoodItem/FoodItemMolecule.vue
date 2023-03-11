f2ba8e12a70d52cefb367f802632610c73924862
Tim <timothee.varaillas@gmail.com>
2023-03-11 16:32:56 +0100

    @ Modification mineure

--- src/components/molecules/FoodItem/FoodItemMolecule.vue
+++ src/components/molecules/FoodItem/FoodItemMolecule.vue
@@ -13,7 +13,7 @@ const props = defineProps<{
     class="p-6 flex items-center justify-start border-2 border-slate-300 rounded-lg hover:border-amber-400 hover:ring-4 hover:ring-amber-50 hover:cursor-pointer sm:col-span-12 md:col-span-12 lg:col-span-6 xl:col-span-4 2xl:col-span-3"
   >
     <image-atom :src="`/img/food/${props.image}`" height="40px" width="40px" />
-    <text-atom class="ml-4 text-slate-700" size="xl" weight="bold">
+    <text-atom class="ml-4" size="xl" weight="bold">
       {{ props.name }}
     </text-atom>
   </div>
