28c1ea472d6499511463c64f6b760470eb8c6fff
Tim <timothee.varaillas@gmail.com>
2023-03-11 12:29:06 +0100

    @ Utilisation de l'atome Title dans l'organisme FoodGrid

--- src/components/organisms/FoodGrid/FoodGridOrganism.vue
+++ src/components/organisms/FoodGrid/FoodGridOrganism.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
+import TitleAtom from "@/components/atoms/Title/TitleAtom.vue";
 import FoodItemMolecule from "@/components/molecules/FoodItem/FoodItemMolecule.vue";
-import TextAtom from "@/components/atoms/Text/TextAtom.vue";
 
 import type { FoodsByCategory } from "@/types/food.type";
 
@@ -15,9 +15,9 @@ const props = defineProps<{
     v-for="category in props.foodByCategories"
     :key="category.category"
   >
-    <text-atom class="col-span-12 text-center mb-6" size="3xl" weight="bold">
+    <title-atom class="col-span-12 text-center mb-6" :level="2">
       {{ category.category }}
-    </text-atom>
+    </title-atom>
     <food-item-molecule
       v-for="item in category.foods"
       :key="item._id"
