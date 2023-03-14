38320e174e75c7ab2f9ed3f4491fe2b20b7b1135
Tim <timothee.varaillas@gmail.com>
2023-03-14 16:06:38 +0100

    + Création de l'atome NavBarMenuItem

---
+++ src/components/atoms/NavBarMenuItem/NavBarMenuItemAtom.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { NavBarMenuItem } from "@/types/menu.type";
+
+const props = defineProps<{
+  menuItem: NavBarMenuItem;
+}>();
+</script>
+
+<template>
+  <li class="mr-6 text-slate-800 font-medium hover:text-amber-500">
+    <router-link
+      :to="props.menuItem.url"
+      active-class="text-amber-500 font-extrabold"
+    >
+      {{ props.menuItem.name }}
+    </router-link>
+  </li>
+</template>
+
+<style scoped lang="scss"></style>
