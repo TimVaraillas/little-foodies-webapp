723b36d7ce235c5d700f00191b0ea7adbd1fe25c
Tim <timothee.varaillas@gmail.com>
2023-03-14 16:07:00 +0100

    + Création de la molécule NavBarOrganism

---
+++ src/components/molecules/NavBarMenu/NavBarMenuMolecule.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import NavBarMenuItemAtom from "@/components/atoms/NavBarMenuItem/NavBarMenuItemAtom.vue";
+
+import type { NavBarMenuItem } from "@/types/menu.type";
+
+const props = defineProps<{
+  menu: NavBarMenuItem[];
+}>();
+</script>
+
+<template>
+  <ul class="h-full grow flex items-center p-4">
+    <nav-bar-menu-item-atom
+      v-for="item in props.menu"
+      :key="item.name"
+      :menu-item="item"
+    />
+  </ul>
+</template>
+
+<style scoped lang="scss"></style>
