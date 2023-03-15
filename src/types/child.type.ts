745090b8794ea398d75fd3f28471246a9feeb59b
Tim <timothee.varaillas@gmail.com>
2023-03-15 18:01:28 +0100

    + Création du type Child

---
+++ src/types/child.type.ts
@@ -0,0 +1,10 @@
+import type { User } from "./user.type";
+
+export interface Child {
+  _id?: string;
+  first_name: string;
+  last_name: string;
+  gender: "masculine" | "feminine";
+  birthday: Date;
+  user: User;
+}
