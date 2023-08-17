6e5ea522b60071cc9ee743650afa6419db601529
Timothée VARAILLAS <timothee.varaillas@legrand.com>
2023-08-17 18:01:50 +0200

    Update InputTextAtom component

--- src/components/atoms/InputText/InputTextAtom.vue
+++ src/components/atoms/InputText/InputTextAtom.vue
@@ -11,6 +11,7 @@ interface Props {
   prependIcon?: string;
   clearable?: boolean;
   size?: Size;
+  error?: string;
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -28,7 +29,7 @@ const clearValue = () => {
   internalValue.value = "";
 };
 
-const inputClass = {
+const inputSizeClass = {
   "px-3 py-1 text-xs": props.size === "xs",
   "px-3 py-1.5 text-sm": props.size === "sm",
   "px-4 py-2.5 text-base": props.size === "md",
@@ -38,30 +39,46 @@ const inputClass = {
 </script>
 
 <template>
-  <div
-    class="w-full flex bg-slate-50 text-slate-900 rounded-lg border border-slate-300 focus-within:ring-4 focus-within:ring-amber-200"
-    :class="inputClass"
-  >
+  <div>
     <div
-      v-if="props.prependIcon"
-      class="flex items-center pointer-events-none mr-3 text-slate-400"
+      class="w-full flex bg-slate-50 text-slate-900 rounded-lg border border-slate-300 focus-within:ring-4 focus-within:ring-amber-200"
+      :class="{
+        ...inputSizeClass,
+        'bg-rose-50 text-rose-600 border-rose-300 focus-within:ring-rose-200':
+          error,
+      }"
     >
-      <icon-atom :icon="props.prependIcon" />
+      <div
+        v-if="props.prependIcon"
+        class="flex items-center pointer-events-none mr-3 text-slate-400"
+        :class="{
+          'text-rose-600': error,
+        }"
+      >
+        <icon-atom :icon="props.prependIcon" />
+      </div>
+      <input
+        type="text"
+        class="grow bg-slate-50"
+        :class="{
+          'bg-rose-50': error,
+        }"
+        v-model="internalValue"
+        :placeholder="props.placeholder"
+      />
+      <button
+        v-if="props.clearable && internalValue"
+        type="button"
+        class="flex items-center ml-2 text-slate-400 hover:text-slate-800"
+        :class="{
+          'text-rose-600 hover:text-rose-800': error,
+        }"
+        @click="clearValue"
+      >
+        <icon-atom icon="xmark" />
+      </button>
     </div>
-    <input
-      type="text"
-      class="grow bg-slate-50"
-      v-model="internalValue"
-      :placeholder="props.placeholder"
-    />
-    <button
-      v-if="props.clearable && internalValue"
-      type="button"
-      class="flex items-center ml-2 text-slate-400 hover:text-slate-800"
-      @click="clearValue"
-    >
-      <icon-atom icon="xmark" />
-    </button>
+    <small class="mx-2 text-rose-600" v-if="error">{{ error }}</small>
   </div>
 </template>
 
