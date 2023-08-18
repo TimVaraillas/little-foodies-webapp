3f464721c2a3b2fba98ee4d88aa3ab66599941a2
Timothée VARAILLAS <timothee.varaillas@legrand.com>
2023-08-18 14:31:18 +0200

    update FamilyPage component

--- src/components/pages/Family/FamilyPage.vue
+++ src/components/pages/Family/FamilyPage.vue
@@ -2,13 +2,13 @@
 import { Ref, ref, onMounted } from "vue";
 import { storeToRefs } from "pinia";
 
-import AvatarAtom from "@/components/atoms/Avatar/AvatarAtom.vue";
 import CardAtom from "@/components/atoms/Card/CardAtom.vue";
 import IconAtom from "@/components/atoms/Icon/IconAtom.vue";
 import TextAtom from "@/components/atoms/Text/TextAtom.vue";
 import TitleAtom from "@/components/atoms/Title/TitleAtom.vue";
 import GridItemAtom from "@/components/atoms/GridItem/GridItemAtom.vue";
 import DrawerMolecule from "@/components/molecules/Drawer/DrawerMolecule.vue";
+import ModalMolecule from "@/components/molecules/Modal/ModalMolecule.vue";
 import GridMolecule from "@/components/molecules/Grid/GridMolecule.vue";
 import HeaderOrganism from "@/components/organisms/Header/HeaderOrganism.vue";
 import ChildFormOrganism from "@/components/organisms/ChildForm/ChildFormOrganism.vue";
@@ -28,6 +28,7 @@ const { children } = storeToRefs(childStore);
 
 const childForm = ref<InstanceType<typeof ChildFormOrganism> | null>(null);
 const drawerOpen: Ref<boolean> = ref(false);
+const deleteChildModalOpen: Ref<boolean> = ref(false);
 
 const onCloseDrawer = () => {
   drawerOpen.value = false;
@@ -58,18 +59,25 @@ const addChild = async (value: any) => {
     toastStore.success({ text: "Votre enfant a été ajouté avec succès" });
   }
 };
+
+const deleteChild = () => {
+  deleteChildModalOpen.value = false;
+};
 </script>
 
 <template>
   <header-organism title="Ma famille"></header-organism>
   <div class="container mx-auto">
     <div class="my-10">
-      <title-atom class="text-center mb-8" :level="2">Parents</title-atom>
+      <title-atom class="text-center mb-8" :level="2">Utilisateur</title-atom>
       <grid-molecule :columns="12" gap="16px">
         <grid-item-atom :span="6">
           <card-atom>
             <div class="flex items-center justify-start">
-              <avatar-atom />
+              <icon-atom
+                icon="user"
+                class="flex justify-center items-center text-xl text-amber-500 border-amber-500 border rounded-full w-10 h-10"
+              ></icon-atom>
               <text-atom class="ml-4" size="xl" weight="bold">
                 {{ user?.first_name }} {{ user?.last_name }}
               </text-atom>
@@ -83,20 +91,27 @@ const addChild = async (value: any) => {
       <grid-molecule :columns="12" gap="16px">
         <grid-item-atom v-for="child in children" :key="child._id" :span="6">
           <card-atom>
-            <div class="flex items-center justify-start">
+            <div class="flex items-center justify-between">
+              <div class="flex items-center justify-start">
+                <icon-atom
+                  v-if="child.gender === 'masculine'"
+                  icon="child"
+                  class="flex justify-center items-center text-2xl text-blue-500 border-blue-500 border rounded-full w-10 h-10"
+                ></icon-atom>
+                <icon-atom
+                  v-if="child.gender === 'feminine'"
+                  icon="child-dress"
+                  class="flex justify-center items-center text-2xl text-pink-500 border-pink-500 border rounded-full w-10 h-10"
+                ></icon-atom>
+                <text-atom class="ml-4" size="xl" weight="bold">
+                  {{ child?.first_name }} {{ child?.last_name }}
+                </text-atom>
+              </div>
               <icon-atom
-                v-if="child.gender === 'masculine'"
-                icon="child"
-                class="text-2xl text-blue-500"
+                icon="trash"
+                class="flex justify-center items-center text-xl text-slate-300 hover:text-rose-500 hover:cursor-pointer"
+                @click.stop="deleteChildModalOpen = true"
               ></icon-atom>
-              <icon-atom
-                v-if="child.gender === 'feminine'"
-                icon="child-dress"
-                class="text-2xl text-pink-500"
-              ></icon-atom>
-              <text-atom class="ml-4" size="xl" weight="bold">
-                {{ child?.first_name }} {{ child?.last_name }}
-              </text-atom>
             </div>
           </card-atom>
         </grid-item-atom>
@@ -116,6 +131,7 @@ const addChild = async (value: any) => {
       </grid-molecule>
     </div>
   </div>
+
   <drawer-molecule :is-open="drawerOpen" @close="onCloseDrawer">
     <template #title>
       <div class="text-amber-500">Ajouter un enfant</div>
@@ -125,6 +141,19 @@ const addChild = async (value: any) => {
       @submit="addChild"
     ></child-form-organism>
   </drawer-molecule>
+
+  <modal-molecule
+    :is-open="deleteChildModalOpen"
+    confirm-button-text="Supprimer"
+    @cancel="deleteChildModalOpen = false"
+    @confirm="deleteChild"
+  >
+    <template #title>
+      <div class="text-rose-500">Confirmer la suppression</div>
+    </template>
+    Voulez-vous vraiment supprimer cet enfant ? <br />
+    Toutes les données relatives à cet enfant seront supprimée.
+  </modal-molecule>
 </template>
 
 <style scoped lang="scss"></style>
