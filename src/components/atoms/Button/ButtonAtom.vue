1bbc898cbfc105c6690f2a334eefb3edd539d1be
Timothée VARAILLAS <timothee.varaillas@legrand.com>
2023-08-18 14:29:41 +0200

    Add color management to ButtonAtom component

--- src/components/atoms/Button/ButtonAtom.vue
+++ src/components/atoms/Button/ButtonAtom.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 type Size = "xs" | "sm" | "md" | "lg" | "xl";
+type Color = "primary" | "default" | "info" | "success" | "warning" | "error";
 
 interface Props {
   modelValue?: string;
   prependIcon?: string;
   size?: Size;
+  color: Color;
 }
 
 const props = withDefaults(defineProps<Props>(), {
   size: "md",
+  color: "default",
 });
 
 const inputClass = {
@@ -17,16 +20,78 @@ const inputClass = {
   "px-4 py-2.5 text-base": props.size === "md",
   "pl-4 pr-5 py-4 text-lg": props.size === "lg",
   "pl-4 pr-5 py-6 text-xl": props.size === "xl",
+  [props.color]: true,
 };
 </script>
 
 <template>
   <button
-    class="focus:outline-none text-white bg-amber-500 hover:bg-amber-400 focus:ring-4 focus:ring-amber-200 font-medium rounded-lg"
+    class="button cursor-pointer focus:outline-none focus:ring-4 font-medium rounded-lg"
     :class="inputClass"
   >
     <slot></slot>
   </button>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@mixin colorButton($textColor, $bgColor, $hoverBgColor, $ringColor) {
+  color: $textColor;
+  background-color: $bgColor;
+  &:hover {
+    background-color: $hoverBgColor;
+  }
+  &:focus {
+    --tw-ring-color: $ringColor;
+  }
+}
+.button {
+  &.primary {
+    @include colorButton(
+      theme("colors.white"),
+      theme("colors.amber.500"),
+      theme("colors.amber.400"),
+      theme("colors.amber.200")
+    );
+  }
+  &.default {
+    @include colorButton(
+      theme("colors.white"),
+      theme("colors.slate.400"),
+      theme("colors.slate.300"),
+      theme("colors.slate.200")
+    );
+  }
+  &.info {
+    @include colorButton(
+      theme("colors.white"),
+      theme("colors.sky.500"),
+      theme("colors.sky.400"),
+      theme("colors.sky.200")
+    );
+  }
+  &.success {
+    @include colorButton(
+      theme("colors.white"),
+      theme("colors.emerald.500"),
+      theme("colors.emerald.400"),
+      theme("colors.emerald.200")
+    );
+  }
+  &.warning {
+    @include colorButton(
+      theme("colors.white"),
+      theme("colors.amber.500"),
+      theme("colors.amber.400"),
+      theme("colors.amber.200")
+    );
+  }
+  &.error {
+    @include colorButton(
+      theme("colors.white"),
+      theme("colors.rose.500"),
+      theme("colors.rose.400"),
+      theme("colors.rose.200")
+    );
+  }
+}
+</style>
